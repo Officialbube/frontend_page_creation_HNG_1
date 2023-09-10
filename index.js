cfc1044521efba3e9fdc0dc6da3f68bd465a2706
officialbube <ebubenkereebube@gmail.com>
2023-09-10 12:05:23 +0100

    updated

---
+++ index.js
@@ -0,0 +1,25 @@
+const dayOfTheWeek = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+
+  const today = new Date();
+  const currentTime = Date.now();
+  const day = today.getDay();
+  const dayList = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  dayOfTheWeek.innerHTML = dayList[day];
+
+  const updateTime = () => {
+    const newTime = Date.now();
+    document.querySelector(
+      '[data-testid="currentUTCTime"]'
+    ).textContent = newTime;
+  };
+  setInterval(updateTime, 1);
